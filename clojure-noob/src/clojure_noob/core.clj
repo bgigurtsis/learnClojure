e65abca442dd3270aa2b114a79672d7d4192c0a7
Billy Gigurtsis <bgigurtsis@gmail.com>
2021-02-24 16:00:56 +0000

    add second if statement

--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -1,12 +1,15 @@
 (ns clojure-noob.core
   (:gen-class))
 
+;define function main
 (defn -main
+  ;define argument 'severity'
   [severity]
   (str "Disaster We're "
     (if (= severity :mild)
-      "Mildly inconvenienced"
-      "Doomed")))
-
+      "Mildly inconvenienced")
+    (if (= severity :severe)
+      "Doomed"
+      "ok?")))
 
 (-main :mild)
