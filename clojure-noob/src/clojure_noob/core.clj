e9c376186bb5e1194952a904095359143645d681
Billy Gigurtsis <bgigurtsis@gmail.com>
2022-01-31 13:20:38 +0000

    clj

--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -1,5 +1,19 @@
 (ns clojure-noob.core
   (:gen-class))
 
+;define function main
+(defn main [severity]
+  (println (str "We're")
+    (if (= severity :mild)
+      "mildly inconvenienced."
+      (if (= severity :severe)
+        "doomed."
+        "OK?"))))
+
+(main :mild)
+
+;define main function, same as the one above
 (defn main [severity]
   (println "We're" (severity {:mild "mildly inconvenienced." :severe "doomed."} "OK.")))
+
+(main :severe)
